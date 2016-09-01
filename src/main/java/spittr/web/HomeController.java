ad0dac5a385dc165c9275cf3405f1be00042679c
Felipe de Morais <felipejpa15@gmail.com>
2016-09-01 01:21:32 -0300

    start the project to setup Spring app

---
+++ src/main/java/spittr/web/HomeController.java
@@ -0,0 +1,14 @@
+package spittr.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+@RequestMapping(value = "/", method = GET)
+public class HomeController {
+    public String home() {
+        return "home";
+    }
+}
